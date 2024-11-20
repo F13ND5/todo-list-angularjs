dcc25447be7b246583b64e36c2da3320ed459168
F13ND5 <112640850+F13ND5@users.noreply.github.com>
2024-11-20 18:23:05 +0000

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,6 @@
+
+export default {
+  basePath: 'https://F13ND5.github.io/todo-list-angularjs/',
+  entryPoints: new Map([['', () => import('./main.server.mjs')]]),
+};
+  
---
+++ server/assets-chunks/index_html.mjs
@@ -0,0 +1,13 @@
+export default `<!DOCTYPE html><html lang="en" data-beasties-container><head>
+  <meta charset="utf-8">
+  <title>TodoListApp</title>
+  <base href="https://F13ND5.github.io/todo-list-angularjs/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-5INURTSO.css"><style ng-app-id="ng">.container[_ngcontent-ng-c3773336384]{max-width:600px;margin:0 auto;padding:20px;text-align:center}button[_ngcontent-ng-c3773336384]{margin-left:10px;padding:5px 10px}button[_ngcontent-ng-c3773336384]:disabled{background-color:#ccc}input[_ngcontent-ng-c3773336384]{padding:5px;width:80%}ul[_ngcontent-ng-c3773336384]{list-style:none;padding:0}li[_ngcontent-ng-c3773336384]{margin:10px 0}.completed[_ngcontent-ng-c3773336384]{text-decoration:line-through;color:gray}</style></head>
+<body><!--nghm--><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
+</script><script>window.__jsaction_bootstrap(document.body,"ng",["input","compositionstart","compositionend","click"],["blur"]);</script>
+  <app-root _nghost-ng-c3773336384 ng-version="19.0.0" ngh="0" ng-server-context="ssg"><div _ngcontent-ng-c3773336384 class="container"><h1 _ngcontent-ng-c3773336384>To-Do List App</h1><div _ngcontent-ng-c3773336384><input _ngcontent-ng-c3773336384 type="text" placeholder="Add a new task" value class="ng-untouched ng-pristine ng-valid" jsaction="input:;blur:;compositionstart:;compositionend:;"><button _ngcontent-ng-c3773336384 jsaction="click:;"> Add Task </button></div><ul _ngcontent-ng-c3773336384 id="taskList"><!----></ul></div></app-root>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-3L3KE5DN.js" type="module"></script>
+
+<script id="ng-state" type="application/json">{"__nghData__":[{"t":{"8":"t0"},"c":{"8":[]}}]}</script></body></html>`;
